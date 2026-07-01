bb94b4d991c2298277f4b5e614f5765c6df117d2
Gustavo HGP <gustavo.hgp.dev@gmail.com>
2026-07-01 15:58:12 -0300

    initial setup

---
+++ Models/InventoryItem.cs
@@ -0,0 +1,8 @@
+﻿namespace DotNetGameInventoryAPI;
+
+public class InventoryItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+}
