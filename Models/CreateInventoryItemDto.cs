fca0637ae4f1dec9d287ac96c9a368976525692f
Gustavo HGP <gustavo.hgp.dev@gmail.com>
2026-09-09 17:45:37 -0300

    POST input validation

--- Models/CreateInventoryItemDto.cs
+++ Models/CreateInventoryItemDto.cs
@@ -1,7 +1,12 @@
-﻿namespace DotNetGameInventoryAPI.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetGameInventoryAPI.Models;
 
 public class CreateInventoryItemDto
 {
+    [Required]
+    [StringLength(100, MinimumLength = 1)]
     public string Name { get; set; } = string.Empty;
+    [Range(0,999)]
     public int Quantity { get; set; }
 }
